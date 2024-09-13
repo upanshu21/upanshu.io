e4fd34b6d0d85b071104796cf87e1e0e2ff86dde
upanshu <upanshu21@gmail.com>
2024-09-13 10:40:06 -0700

    minor corrections

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -10,7 +10,7 @@ export default function Timeline({ defaultColor }) {
                         <div class="relative pl-8 sm:pl-32 py-6 group">
                             <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                                 <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Present</time>
-                                <div class="font-medium text-gray-200 mb-1 sm:mb-0">Graduate Teaching Assistant and Researcher</div>
+                                <div class="font-medium text-gray-200 mb-1 sm:mb-0">Graduate Teaching Assistant and Researcher at OSU</div>
                             </div>
                         </div>
                         
@@ -20,7 +20,7 @@ export default function Timeline({ defaultColor }) {
                                 <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">SEP, 2022</time>
                                 <div class="font-medium text-gray-200 mb-1 sm:mb-0">Graduate Computer Science student</div>
                                 </div>
-                            <div class="text-slate-500">Secured Fully funded Master's from Oregon State University for Computer Science.</div>
+                            <div class="text-slate-500">Secured Fully funded Master's in Computer Science from Oregon State University.</div>
                         </div>
                         
                         <div class="relative pl-8 sm:pl-32 py-6 group">
@@ -28,20 +28,20 @@ export default function Timeline({ defaultColor }) {
                                 <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">July, 2022</time>
                                 <div class="font-medium text-gray-200 mb-1 sm:mb-0">Software Engineer at Ultimate Kronos Group</div>
                                 </div>
-                            <div class="text-slate-500">Part of off-shore team for Notifications platform team for UKG form Knoldus's side. </div>
+                            <div class="text-slate-500">Part of off-shore team for Notifications platform team for UKG from Knoldus's side. </div>
                         </div>
 
                         <div class="relative pl-8 sm:pl-32 py-6 group">
                             <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                                 <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">July, 2022</time>
-                                <div class="font-medium text-gray-200 mb-1 sm:mb-0">Promoted</div>
+                                <div class="font-medium text-gray-200 mb-1 sm:mb-0">Promoted to Software Engineer</div>
                                 </div>
                         </div>
                         
                         <div class="relative pl-8 sm:pl-32 py-6 group">
                             <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                                 <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Jan, 2020</time>
-                                <div class="font-medium text-gray-200 mb-1 sm:mb-0">Software Engineer Intern</div>
+                                <div class="font-medium text-gray-200 mb-1 sm:mb-0">Software Engineer Intern at Knoldus Inc.</div>
                                 </div>
                             <div class="text-slate-500">Part of Digital Asset team for building decentralized loan-lending application.</div>
                         </div>
@@ -51,12 +51,14 @@ export default function Timeline({ defaultColor }) {
                                 <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Jan, 2020</time>
                                 <div class="font-medium text-gray-200 mb-1 sm:mb-0">Graduated from Amity University</div>
                                 </div>
+                                <div class="text-slate-500">Degree: Bachelor's in Computer Science and Engineering.</div>
+
                         </div>
 
                         <div class="relative pl-8 sm:pl-32 py-6 group">
                             <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                                 <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">July, 2016</time>
-                                <div class="font-medium text-gray-200 mb-1 sm:mb-0">Stared Undergrad Degree in Computer Science</div>
+                                <div class="font-medium text-gray-200 mb-1 sm:mb-0">Stared Undergrad Degree in Computer Science at Amity University</div>
                                 </div>
                         </div>
     
